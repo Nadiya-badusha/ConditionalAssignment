951801f126693b51ae3807bbf01490d5b0d803d1
Nadiya-badusha <nadiyabadusha129@gmail.com>
2025-05-15 17:49:18 +0530

    Add files via upload

---
+++ PROJECT6/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using System;
+
+namespace PROJECT6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        // Write a program in C# which is a Menu-Driven Program
+        //to perform a simple calculation
+
+
+            #region switch
+            //mathamatical operations
+            int firstNumber = 0, secondNumber = 0;
+            char continuechoice = 'n';
+
+            do
+            {
+
+                //get input from user
+                Console.WriteLine("enter first value");
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("enter second value");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                //show the actions
+                Console.WriteLine("1.addition");
+                Console.WriteLine("2.difference");
+                Console.WriteLine("3.multiplication");
+                Console.WriteLine("4.Division");
+
+                //get choice
+                Console.Write("enter your choice(1-4):");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+
+
+                //process output
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("sum is:" + (firstNumber + secondNumber));
+                        break;
+
+                    case 2:
+                        Console.WriteLine("the difference: " + (secondNumber - firstNumber));
+                        break;
+                    case 3:
+                        Console.WriteLine("the product is :" + (firstNumber * secondNumber));
+                        break;
+                    case 4:
+                        Console.WriteLine("the quotient is: " + (secondNumber / firstNumber));
+                        break;
+                    default:
+                        Console.WriteLine("invalid choice");
+                        break;
+
+
+
+                }
+                //ask if we want to continue
+                Console.Write("do you want to contimue? \n (y to continue,n to exit)");
+                continuechoice = Convert.ToChar(Console.ReadLine().ToLower());
+            }
+            while (continuechoice == 'y');
+
+
+            #endregion
+
+
+            //pause the key
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PROJECT7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace PROJECT7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program in C# to calculate and print the
+            //Electricity bill of a given customer.The customer id.,
+            //name and unit consumed by the user should be taken
+            //from the keyboard and display the total amount to pay
+            //to the customer.The charge are as follow
+
+            //read customer id
+
+            Console.WriteLine("enter customer ID");
+            int custId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            //read name
+            Console.WriteLine("enter name");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+
+            //read unit consumed
+            Console.WriteLine("enter unit consumed by user");
+            int unit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            //check price per unit and pirnt the total amount to be payed
+            if (unit >= 600)
+            {
+
+                Console.WriteLine("the total amount to pay is {0}", 2 * unit);
+            }
+            else if (unit < 600 && unit>400)
+            {
+                Console.WriteLine("the total amount to pay is {0}", 1.8 * unit);
+
+            }else if (unit < 400 && unit>200)
+            {
+                Console.WriteLine("the total amount to pay is {0}", 1.5 * unit);
+
+            }else if(unit <199)
+            {
+                Console.WriteLine("the total amount to pay is {0}", 1.2 * unit);
+
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+
+        }
+    }
+}
